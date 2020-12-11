fceb16599c43838ce7e2b19a321ff3c3f7a222dc
nfcarbone <nfcarbone@gmail.com>
2020-12-11 18:12:13 -0500

    working

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
-
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
+});
+
+app.post('/api/fileanalyse/', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  return res.json({ name: originalname, type: mimetype, size });
 });
